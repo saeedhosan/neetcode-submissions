eb97726c24e5d9307a30e71d36ee22521fc6faaa
Saeed Hosan <saeedhosansh@gmail.com>
2026-08-31 09:00:49 +0600

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        //same length
+        //frequencies
+        //compare
+
+        if (s.length !== t.length) return false;
+
+        const freq = {};
+
+        for (let i = 0; i < s.length; i++) {
+            freq[s[i]] = (freq[s[i]] || 0) + 1;
+            freq[t[i]] = (freq[t[i]] || 0) - 1;
+        }
+
+        for(const char in freq){
+            if(freq[char] !== 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
