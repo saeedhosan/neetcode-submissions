084fbfd85bfd750638bd4145c0ec0f2f68c4ec89
Saeed Hosan <saeedhosansh@gmail.com>
2026-09-01 20:41:43 +0600

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = "";
+        for (const str of strs) {
+            res += str.length + "#" + str;
+        }
+        return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const res = [];
+
+        if (str.length === 0) return res;
+
+        let slow = 2;
+        let fast = parseInt(str[0]) + 2;
+
+        while (slow <= str.length) {
+            let char = "";
+
+            while (slow < fast) {
+                char += str[slow];
+                slow++;
+            }
+
+            res.push(char);
+
+            if (slow >= str.length) break;
+
+            let len = parseInt(str[slow]);
+
+            slow += 2;
+            fast = slow + len;
+        }
+
+        return res;
+    }
+}
