e9438167eed8f8596e8bea9b3f23fe841af89618
Saeed Hosan <saeedhosansh@gmail.com>
2026-09-03 13:35:53 +0600

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        //pointers
+        //max water
+        //calculate
+        //save max
+        //moves
+        //Time: O(n)
+        //Space: O(1)
+
+        let left = 0;
+        let right = heights.length - 1;
+        let maxWater = 0;
+
+        while (left < right) {
+            const width = right - left;
+            const height = Math.min(heights[left], heights[right]);
+            const waters = width * height;
+
+            maxWater = Math.max(maxWater, waters);
+
+            if (heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+}
