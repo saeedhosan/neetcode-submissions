cb8b48e7e7ce518a2bed194b0b0d2e62647f8e01
Saeed Hosan <saeedhosansh@gmail.com>
2026-09-06 13:31:41 +0600

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        // sliding window
+        // frequency
+        // max frequency
+        // window length
+        // replacements
+        // move slow
+        // max window
+        // Time:  O(n)
+        // Space: O(26) = O(1)
+
+        let res = 0;
+        let slow = 0;
+        let freq = {};
+        let maxf = 0;
+
+        for (let fast = 0; fast < s.length; fast++) {
+            const char = s[fast];
+
+            freq[char] = (freq[char] || 0) + 1;
+
+            maxf = Math.max(maxf, freq[char]);
+
+            let wind = fast - slow + 1;
+
+            while (wind - maxf > k) {
+                freq[s[slow]]--;
+                slow++;
+                wind = fast - slow + 1;
+            }
+
+            res = Math.max(res, wind);
+        }
+
+        return res;
+    }
+}
