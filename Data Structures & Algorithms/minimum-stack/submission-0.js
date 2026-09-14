36052b9f738f40d490295c247de70b46186dcaed
Saeed Hosan <saeedhosansh@gmail.com>
2026-09-14 12:39:52 +0600

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,47 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+
+        // If minStack is empty, this value is the minimum
+        if (this.minStack.length === 0) {
+            this.minStack.push(val);
+            return;
+        }
+        
+        // Find the smaller value
+        const newMin = Math.min(this.getMin(), val);
+
+        this.minStack.push(newMin);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
