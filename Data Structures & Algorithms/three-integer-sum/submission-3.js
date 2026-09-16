e1a969c66c033e15788109300e524c03f116d8e6
Saeed Hosan <saeedhosansh@gmail.com>
2026-09-16 13:14:07 +0600

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a, b) => a - b);
+        const res = [];
+
+        for(let i = 0; i < nums.length; i++){
+
+            //we need to skip duplicate e.g [1, 1]
+            if(i > 0 && nums[i] === nums[i - 1]){
+                continue;
+            }
+
+            let l = i + 1;
+            let r = nums.length - 1;
+
+            while(l < r){
+                const sum = nums[i] + nums[l] + nums[r];
+
+                if(sum > 0){
+                    r--;
+                }else if(sum < 0){
+                    l++;
+                }else{
+                    //we found the result item
+                    res.push([nums[i], nums[l], nums[r]]);
+                    l++;
+                    r--;
+
+                    //skip left pointer duplicate
+                    while(l < r && nums[l] == nums[l - 1]){
+                        l++;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
